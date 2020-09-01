ef5edd0d0bbae086882dbab6c35d4ead3ad750c2
leeyongjoo <2yongjooo@gmail.com>
2020-09-01 15:32:00 +0900

    feat: language_code 추가

--- modules/languages.py
+++ modules/languages.py
@@ -5,6 +5,12 @@ extensions = {
     'java': '.java',
 }
 
+language_code = {
+    'c': 0,
+    'c++': 1,
+    'java': 3,
+    'python': 6,
+}
 
 def get_languages():
     return list(extensions.keys())
