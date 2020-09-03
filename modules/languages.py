ffac54c49e1d82248b27cdf127247c17aaad57a8
leeyongjoo <2yongjooo@gmail.com>
2020-09-03 17:04:18 +0900

    style: languages_code -> codeup_language_code 변경
    함수명도 변경

--- modules/languages.py
+++ modules/languages.py
@@ -5,16 +5,18 @@ extensions = {
     'java': '.java',
 }
 
-language_code = {
+
+def get_extension(language):
+    return extensions[language]
+
+
+codeup_language_code = {
     'c': 0,
     'c++': 1,
     'java': 3,
     'python': 6,
 }
 
-def get_languages():
+
+def get_codeup_languages():
     return list(extensions.keys())
-
-
-def get_extension(language):
-    return extensions[language]
